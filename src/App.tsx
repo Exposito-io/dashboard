2f75db2b3bba5ffb0d863ec4733c40af51ba5370
Mathew <mathew.corm@gmail.com>
2017-07-18 09:05:59 -0400

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -94,6 +94,17 @@ class App extends React.Component {
             <div className="al-content">
               <Main />
             </div>
+            <footer className="al-footer clearfix">
+              <div className="al-footer-right">Created with <i className="ion-heart"></i></div>
+              <div className="al-footer-main clearfix">
+                <div className="al-copy">React Webpack Skeleton</div>
+                <ul className="al-share clearfix">
+                  <li><i className="socicon socicon-facebook"></i></li>
+                  <li><i className="socicon socicon-twitter"></i></li>
+                </ul>
+              </div>
+            </footer>
+        
         </div>
         <Notifications />
       </div>
