120722e11682cbc56731d00c256e26b35a90df2d
Mathew Cormier <mathew.corm@gmail.com>
2017-07-29 19:21:35 -0400

    Animation

--- src/page/new-project/index.tsx
+++ src/page/new-project/index.tsx
@@ -7,12 +7,35 @@ import './main.css'
 
 export class NewProject extends React.Component {
 
+  isAnimating: boolean = false
+  isLastStep: boolean = false
+  currentEntry: HTMLElement
+  fieldsList: HTMLElement
+
+  constructor(props: any) {
+    super(props)
+
+    //setTimeout(() => this.init(), 1000)
+
+    setTimeout(() => this.nextEntry(), 2000)
+  }
+
+  componentDidMount() {
+    this.currentEntry = document.getElementById('entry1')
+    this.fieldsList = document.getElementById('field-list')   
+  }
+
   renderBreadcrumbs() {
     return (
       <Breadcrumbs>
 
       </Breadcrumbs>
     )
+  }
+
+  nextEntry() {
+    this.fieldsList.classList.add('fs-display-next')
+    this.currentEntry.classList.add('fs-hide')
   }
 
   render() {
@@ -22,13 +45,13 @@ export class NewProject extends React.Component {
           Lorem Ipsum
         </Panel>
 
-        <div className="new-project">
+        <div className="new-project fs-form">
             <h2 className="main-title">New project</h2>
 
-            <div className="form">
-              <div className="entry">
-                <h2>Select a project name</h2>
-                <input type="text" placeholder="My new app" />
+            <div className="form fs-fields" id="field-list">
+              <div className="entry" id="entry1">
+                <h2 className="fs-anim-upper">Select a project name</h2>
+                <input className="fs-anim-lower" type="text" placeholder="My new app" />
               </div>
             </div>
         </div>
