8afdc9ebd3d3f2a984d44ba84aec459b92bbbb80
Mathew Cormier <mathew.corm@gmail.com>
2017-08-29 22:29:54 -0400

    Chart colors

--- src/page/new-project/components/equity-chart/index.tsx
+++ src/page/new-project/components/equity-chart/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { Panel } from 'react-blur-admin'
-import { PieChart, Pie } from 'recharts'
+import { PieChart, Pie, Cell } from 'recharts'
 import { bind } from 'bind-decorator'
 import { setRef } from '../../../../lib/tools'
 
@@ -9,10 +9,8 @@ import { observer } from 'mobx-react'
 import { NewProjectStore } from '../../new-project-store'
 
 
-const data02 = [{name: 'Group A', value: 2400}, {name: 'Group B', value: 4567},
-                  {name: 'Group C', value: 1398}, {name: 'Group D', value: 9800},
-                  {name: 'Group E', value: 3908}, {name: 'Group F', value: 4800}];
-
+const COLORS = ['#0c3d56', '#2978A0', '#4EA5D9', '#68b8e8', '#FFBB28']
+const UNALLOCATED_COLOR = '#CCC'
 
 const store = NewProjectStore.getStore()
 
@@ -32,16 +30,31 @@ export class EquityChart extends React.Component {
     render() {
         return (
             <div className="equity-chart">
-                <PieChart width={304} height={304}>
-                    <Pie 
-                        activeIndex={[]} 
-                        data={store.equityChartData}
-                        innerRadius={100} 
-                        outerRadius={140} 
-                        fill="#82ca9d"
-                        activeShape={1}
-                    />
-                </PieChart>
+                { store.hasShareholders ?  
+                    <PieChart width={304} height={304}>
+                        <Pie 
+                            activeIndex={[]} 
+                            data={store.equityChartData}
+                            innerRadius={98} 
+                            outerRadius={140} 
+                            fill="#82ca9d"
+                            activeShape={1}
+                            border={'none'}
+                            stroke="none"
+                        >
+                        {
+                            store.equityChartData.map((entry, i) => {
+                                if (entry.name === 'Unallocated')
+                                    return <Cell fill={UNALLOCATED_COLOR}/>
+                                else
+                                    return <Cell fill={COLORS[i % COLORS.length]}/>
+                            })          
+                        }                    
+                        </Pie>
+                    </PieChart>
+                    :
+                    ''
+                }
             </div>
         )
 
