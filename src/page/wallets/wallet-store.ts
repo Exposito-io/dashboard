4ae2e8ced2735144544f89e4ed6a12034c75cfa0
Mathew Cormier <mathew.corm@gmail.com>
2017-10-06 15:55:51 -0400

    walletData

--- src/page/wallets/wallet-store.ts
+++ src/page/wallets/wallet-store.ts
@@ -1,5 +1,10 @@
 import { autorun, observable, computed } from 'mobx'
-import { Wallet } from 'models'
+import { 
+    Wallet,
+    BitcoinWallet,
+    Transaction,
+    PaymentDestination
+} from 'models'
 import { ExpositoClient } from 'exposito-client'
 import { Store } from '../../stores/store'
 import config from '../../config'
@@ -9,7 +14,7 @@ export class WalletStore extends Store {
 
     private static instance: WalletStore
 
-    @observable wallets: Wallet[] = []
+    @observable wallets: { wallet: Wallet, transactions: Transaction[] }[] = []
 
     private client: ExpositoClient
 
@@ -33,7 +38,12 @@ export class WalletStore extends Store {
     private async init() {
         this.client = new ExpositoClient({ url: config.apiUrl, version: config.apiVersion })
 
-        this.wallets = await this.client.wallets.getWallets()
+        this.wallets = (await this.client.wallets.getWallets())
+                                        .map(wallet => ({ wallet: wallet, transactions: [] }))
+
+        this.wallets = [{ wallet: wallet as Wallet, transactions: transactions }].concat(this.wallets)
+
+        this.isLoading = false
     }
 
     /*
@@ -48,3 +58,65 @@ export class WalletStore extends Store {
 
 
 }
+
+
+let wallet = new BitcoinWallet({
+    coreWallet: {},
+    labels: [],
+    name: 'Main Wallet',
+    projectId: 'gge',
+
+})
+
+let t1 = new Transaction()
+
+let transactions: Transaction[] = [
+    {
+        creationDate: new Date('2017-02-04'),
+        endDate: new Date('2017-02-04'),
+        currency: 'BTC',
+        note: 'Transfer from personnal account',
+        amount: '0.53123',
+        sourceType: PaymentDestination.BITCOIN_ADDRESS,
+        sourceWalletId: 'afew',
+        destination: 'awf',
+        destinationType: PaymentDestination.EXPOSITO_WALLET,
+        status: 1
+    },
+    {
+        creationDate: new Date('2017-02-21'),
+        endDate: new Date('2017-02-21'),
+        currency: 'BTC',
+        note: 'Design of the intro page',
+        amount: '-0.145',
+        sourceType: PaymentDestination.BITCOIN_ADDRESS,
+        sourceWalletId: 'afew',
+        destination: 'awf',
+        destinationType: PaymentDestination.EXPOSITO_WALLET,
+        status: 1
+    }, /* 
+  {
+    creationDate: new Date('2017-03-14'),
+    endDate: new Date('2017-03-14'),
+    currency: 'ETH',
+    note: 'Donation',
+    amount: '0.318',
+    sourceType: PaymentDestination.ETHEREUM_ADDRESS,
+    sourceWalletId: 'afew',
+    destination: 'awf',
+    destinationType: PaymentDestination.EXPOSITO_WALLET,
+    status: 1
+  }, */
+    {
+        creationDate: new Date('2017-04-03'),
+        endDate: new Date('2017-04-03'),
+        currency: 'BTC',
+        note: 'Mathew\'s deposit',
+        amount: '0.293',
+        sourceType: PaymentDestination.BITCOIN_ADDRESS,
+        sourceWalletId: 'afew',
+        destination: 'awf',
+        destinationType: PaymentDestination.EXPOSITO_WALLET,
+        status: 1
+    }
+]
