07f635e5f41dfbe16c604387564e2c19bc8b9001
Mathew Cormier <mathew.corm@gmail.com>
2017-12-30 11:55:21 -0500

    Styling add button

--- src/page/wallet-page/pages/periodic-transfers/periodic-transfers.tsx
+++ src/page/wallet-page/pages/periodic-transfers/periodic-transfers.tsx
@@ -54,20 +54,26 @@ export class PeriodicTransfers extends React.Component<Props> {
     render() {
         return (
             <div className="periodic-transfers-section">
-                <svg
-                    onClick={this.createPeriodicTransfer}
-                    className="plus-icon" height="16px" version="1.1" viewBox="0 0 16 16" width="16px"
-                    x="0px"
-                    xmlns="http://www.w3.org/2000/svg"
-                    y="0px">
-                    <path d="M15,6h-5V1c0-0.55-0.45-1-1-1H7C6.45,0,6,0.45,6,1v5H1C0.45,6,0,6.45,0,7v2c0,0.55,0.45,1,1,1h5v5c0,0.55,0.45,1,1,1h2  c0.55,0,1-0.45,1-1v-5h5c0.55,0,1-0.45,1-1V7C16,6.45,15.55,6,15,6z" />
-                </svg>
+
                 <div className="main-container">
-                    <PeriodicTransferList 
-                        list={this.store.periodicPayments} 
-                        selectedPeriodicTransfer={this.store.selectedPeriodicTransfer}
-                        onItemClick={this.onPeriodicTransferClick}
-                    />
+                    <div className="periodic-transfer-list-container">
+                        <div className="add-btn" onClick={this.createPeriodicTransfer}>
+                            <svg                                
+                                className="plus-icon"version="1.1" viewBox="0 0 16 16"
+                                x="0px" y="0px"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path d="M15,6h-5V1c0-0.55-0.45-1-1-1H7C6.45,0,6,0.45,6,1v5H1C0.45,6,0,6.45,0,7v2c0,0.55,0.45,1,1,1h5v5c0,0.55,0.45,1,1,1h2  c0.55,0,1-0.45,1-1v-5h5c0.55,0,1-0.45,1-1V7C16,6.45,15.55,6,15,6z" />
+                            </svg> 
+                            <span>Add a periodic transfer</span>
+                        </div>                    
+                        <PeriodicTransferList 
+                            list={this.store.periodicPayments} 
+                            selectedPeriodicTransfer={this.store.selectedPeriodicTransfer}
+                            onItemClick={this.onPeriodicTransferClick}
+                        />                    
+                    
+                    </div>
                     {this.store.selectedPeriodicTransfer &&
                         <EditPeriodicTransfer 
                             periodicTransfer={this.store.selectedPeriodicTransfer} 
